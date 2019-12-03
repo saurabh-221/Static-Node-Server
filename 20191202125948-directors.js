dfcd6988dc29e742bb73954e80fd5fbb50b42e70
Saurabh Jain <saurabhjain291197@gmail.com>
2019-12-04 05:28:33 +0530

    files added

---
+++ 20191202125948-directors.js
@@ -0,0 +1,27 @@
+'use strict';
+const fs=require('fs')
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('Directors', JSON.parse(fs.readFileSync('./data/direct.json').toString()));
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
---
+++ server.js
@@ -0,0 +1,15 @@
+var static = require('node-static');
+var port = 8080
+var file = new static.Server('./public');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response, function(e, res){
+            if(e && e.status ===404){
+                file.serveFile('/error.html', 404, {}, request, response);
+            }
+        });
+    }).resume();
+}).listen(port, function(){
+    console.log('Server started on port '+port);
+});
